22eab791621ea34bfbef66d0a86819343aabfbf1
arpitbipinbhardwaj <arpitbipinbhardwaj@gmail.com>
2020-10-06 13:58:15 +0530

    login page

---
+++ src/app/module/login/firstpasswordchange/firstpasswordchange.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-firstpasswordchange',
+  templateUrl: './firstpasswordchange.component.html',
+  styleUrls: ['./firstpasswordchange.component.css']
+})
+export class FirstpasswordchangeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/module/login/login/login.component.ts
+++ src/app/module/login/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginForm } from '../../../model/login/loginform.model'
+
 
 @Component({
   selector: 'app-login',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+  private user:LoginForm;
 
   constructor() { }
 
