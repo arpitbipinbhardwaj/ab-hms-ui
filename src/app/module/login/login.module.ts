14563d41d401c8f5487983a90b9bfbfe83d47eb6
arpitbipinbhardwaj <arpitbipinbhardwaj@gmail.com>
2020-10-06 14:19:07 +0530

    login page

--- src/app/module/login/login.module.ts
+++ src/app/module/login/login.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms'
-import { NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { LoginComponent } from './login/login.component';
 import { FirstpasswordchangeComponent } from './firstpasswordchange/firstpasswordchange.component';
@@ -12,8 +11,7 @@ import { FirstpasswordchangeComponent } from './firstpasswordchange/firstpasswor
   declarations: [LoginComponent, FirstpasswordchangeComponent],
   imports: [
     CommonModule,
-    FormsModule,
-    NgForm
+    FormsModule
   ],
   exports: [FormsModule]
 })
